5bad166661188e08253db06a7ff6334b7b1898c4
Gooainekorea <gooaine123@gmail.com>
2025-12-03 14:08:26 +0900

    이상현상

---
+++ 2025_12_03/GPS_arduino/a00_gps.ino
@@ -0,0 +1,77 @@
+#include <SoftwareSerial.h>
+#include <TinyGPSPlus.h>
+
+
+
+TinyGPSPlus gps;
+int RXPin = 13;
+int TXPin = 12;
+SoftwareSerial gpsSerial(RXPin, TXPin);
+// SoftwareSerial mySerial(13, 12); // RX, TX
+
+
+void setup() {
+  Serial.begin(9600);
+  gpsSerial.begin(9600);
+}
+
+void loop() {
+  oled_setup();
+
+    while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+      displayInfo();
+
+  // If 5000 milliseconds pass and there are no characters coming in
+  // over the software serial port, show a "No GPS detected" error
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println("No GPS detected");
+    while (true)
+      ;
+  }
+}
+
+void displayInfo() {
+  if (gps.location.isValid()) {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude: ");
+    Serial.println(gps.altitude.meters());
+  } else {
+    Serial.println("Location: Not Available");
+  }
+
+  Serial.print("Date: ");
+  if (gps.date.isValid()) {
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.println(gps.date.year());
+  } else {
+    Serial.println("Not Available");
+  }
+
+  Serial.print("Time: ");
+  if (gps.time.isValid()) {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.println(gps.time.centisecond());
+  } else {
+    Serial.println("Not Available");
+  }
+
+  Serial.println();
+  Serial.println();
+  delay(1000);
+}
